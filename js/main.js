f88a465d9f43cf2f0727b38faaab32daa41fbe8e
stefancvetincanin <stefancvetincanin@gmail.com>
2019-05-31 11:47:26 +0200

    dodate dummy stranice

--- js/main.js
+++ js/main.js
@@ -1,10 +1,12 @@
 $("#date").on("focus", () => {
-  // console.log("radi")
   $("#lbl-date").hide()
 })
 
 $("#date").on("focusout", () => {
-  // console.log("ne radi")
   if(document.getElementById("date").value === "")
   $("#lbl-date").show()
+})
+
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
 })
